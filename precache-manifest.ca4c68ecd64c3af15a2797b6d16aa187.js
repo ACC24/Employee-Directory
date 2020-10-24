6f583366e18848dc30a2863a186c42b82b0cfe1f
Andreia <chapman.andreia@gmail.com>
2020-10-24 17:34:36 -0400

    Updates

---
+++ precache-manifest.ca4c68ecd64c3af15a2797b6d16aa187.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad3107152198f99834e436e200a29e6",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c9e87eee6bff2f533a8b",
+    "url": "/my-app/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "6b5082c736bcb3524857",
+    "url": "/my-app/static/css/main.45d00be9.chunk.css"
+  },
+  {
+    "revision": "c9e87eee6bff2f533a8b",
+    "url": "/my-app/static/js/2.889c77c6.chunk.js"
+  },
+  {
+    "revision": "c53f65871ab74006a0c4e0b92b224267",
+    "url": "/my-app/static/js/2.889c77c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5082c736bcb3524857",
+    "url": "/my-app/static/js/main.5af7b8aa.chunk.js"
+  },
+  {
+    "revision": "3a8d2cbe73fe7b497cd1",
+    "url": "/my-app/static/js/runtime-main.0d8e8b15.js"
+  }
+]);
