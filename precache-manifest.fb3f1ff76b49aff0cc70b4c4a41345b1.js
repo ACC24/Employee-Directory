4e21c4c8e5c553bc990664c69933f33eafd057dc
Andreia <chapman.andreia@gmail.com>
2020-10-27 18:14:26 -0400

    Updates

---
+++ precache-manifest.fb3f1ff76b49aff0cc70b4c4a41345b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690601f8559c9a7d26a38c2bf539a440",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "c9e87eee6bff2f533a8b",
+    "url": "/Employee-Directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "6b5082c736bcb3524857",
+    "url": "/Employee-Directory/static/css/main.45d00be9.chunk.css"
+  },
+  {
+    "revision": "c9e87eee6bff2f533a8b",
+    "url": "/Employee-Directory/static/js/2.889c77c6.chunk.js"
+  },
+  {
+    "revision": "c53f65871ab74006a0c4e0b92b224267",
+    "url": "/Employee-Directory/static/js/2.889c77c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5082c736bcb3524857",
+    "url": "/Employee-Directory/static/js/main.5af7b8aa.chunk.js"
+  },
+  {
+    "revision": "d071a6317ccce93d22ee",
+    "url": "/Employee-Directory/static/js/runtime-main.0facf412.js"
+  }
+]);
